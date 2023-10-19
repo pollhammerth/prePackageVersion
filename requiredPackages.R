c86f2f0a62394e99f4b2957528b2683ccb3d022c
pollhammerth <32072487+pollhammerth@users.noreply.github.com>
2023-10-19 12:31:27 +0200

    minor

--- requiredPackages.R
+++ requiredPackages.R
@@ -20,8 +20,24 @@ packages = c(
 )
 
 # install all packages
-install.packages(packages)
+for (i in 1:length(packages)) { install.packages(packages[i]) }
 
 # require all packages
 for (i in 1:length(packages)) { eval(parse(text = paste0("require('",packages[i],"')") )) }
+
+
+# these packages are not available for my R version!
+renv::install("rgdal")
+renv::install("maptools")
+renv::install("rgeos")
+utils::install.packages("rgdal")
+utils::install.packages("maptools")
+utils::install.packages("rgeos")
+# all these packages have been removed from cran. Suggested alternatives are sf and terra
+
+
+require(sessioninfo)
+sessionInfo()
+
+
 
